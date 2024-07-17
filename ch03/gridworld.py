f9d33e031367dc26025f1559fc2bba1b82787ee4
nalssee <jinisrolling@gmail.com>
2024-07-17 13:12:27 +0900

    ..

--- ch03/gridworld.py
+++ ch03/gridworld.py
@@ -8,13 +8,14 @@ import numpy as np
 GAMMA = 0.9
 
 
-def is_proper_move(i, move):
+def is_valid_move(i, move):
     if abs(i - move) == 5:
         return 0 <= move < 25
 
     q1, q2 = i // 5, move // 5 
     return q1 == q2 and 0 <= move < 25    
      
+
 def build_coeff_matrices():
     A = np.zeros((25, 25))
     B = np.zeros((25, 1))
@@ -29,7 +30,7 @@ def build_coeff_matrices():
             moves = [i + 1, i + 5, i - 1, i - 5]
             
             for move in moves:
-                if is_proper_move(i, move):
+                if is_valid_move(i, move):
                     A[i, move] += GAMMA 
                 else:
                     A[i, i] += GAMMA
@@ -38,9 +39,31 @@ def build_coeff_matrices():
     # equal probability for all direction
     return 1/4 * A, 1/4 * B
 
-def solve():
+
+def solve_example_5():
     A, B = build_coeff_matrices()
     return np.linalg.inv(A - np.eye(25)) @ (- B)
 
 
-print(solve())
+
+def example8():
+    gamma = 0.9
+    V = np.zeros(25)
+    while True:
+        delta_sum = 0
+        for i in range(25):
+            oldval = V[i]
+            if i == 1:
+                V[i] = 10 + gamma * V[21]
+            elif i == 3:
+                V[i] = 5 + gamma * V[13]
+            else:
+                moves = [i + 1, i + 5, i - 1, i - 5]
+                V[i] = max([gamma * V[m] if is_valid_move(i, m) else -1 + gamma * V[i] for m in moves])
+            delta_sum += abs(oldval - V[i])
+        if delta_sum < 0.001:
+            return V
+
+
+# print(solve_example_5())
+print(example8())
