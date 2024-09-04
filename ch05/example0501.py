afbba7d236a40e3b257432be7c1245f38fd26b3d
nalssee <jinisrolling@gmail.com>
2024-09-04 22:56:48 +0900

    ..

--- ch05/example0501.py
+++ ch05/example0501.py
@@ -1,2 +1,15 @@
 # Blackjack, Monte Carlo
 
+# Constants 
+STICK = 1
+HIT = 0
+
+
+def generate_episode():
+
+    state = init_blackjack()
+
+    while not finished(state):
+        
+        next_state()
+
--- ch06/example0602.py
+++ ch06/example0602.py
@@ -1,95 +1,143 @@
 # %%
 
 import random
+from typing import Callable
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def rms_error(list1: list[float], list2: list[float]) -> float:
+class Config:
+    def __init__(
+        self,
+        update_method="update_tdzero",
+        alpha=0.1,
+        gamma=1.0,
+        n_episodes=100,
+        n_batches=100,
+        V=[0.5] * 5,
+        rmse_series=[],
+    ) -> None:
+
+        self.update_method = update_method
+        self.alpha = alpha
+        self.gamma = gamma
+        self.n_episodes = n_episodes
+
+        self.n_batches = n_batches
+        self.V = V
+        self.rmse_series = rmse_series
+
+    def reset(self):
+        self.V = [0.5] * 5
+        self.rmse_series = []
+
+    def get_update_method(self) -> Callable[[list[int]], None]:
+        return getattr(self, self.update_method)
+
+    def update_monte_carlo(self, episode: list[int]) -> None:
+        for state in episode[:-1]:
+            G = 1 if episode[-1] == 5 else 0
+            self.V[state] += self.alpha * (G - self.V[state])
+
+    def update_tdzero(self, episode: list[int]) -> None:
+        for state, state1 in zip(episode, episode[1:]):
+            reward = 1 if state1 == 5 else 0
+            v1 = 0 if state1 == -1 or state1 == 5 else self.V[state1]
+            self.V[state] += self.alpha * (reward + self.gamma * v1 - self.V[state])
+
+
+def rmse(list1: list[float], list2: list[float]) -> float:
     squared_diff = [(a - b) ** 2 for a, b in zip(list1, list2)]
     mean_squared_diff = sum(squared_diff) / len(squared_diff)
     rms = np.sqrt(mean_squared_diff)
     return rms
 
 
-class RandomWalk:
-    def __init__(self, update_method: str) -> None:
-        self.alpha = 0.1
-        self.gamma = 1.0
-        self.n_episodes = 100
-        self.n_batches = 100
-        self.update_method = update_method 
-        self.value_table = [0.5] * 5
-        
-    def generate_episode(self) -> list[int]:
-        """state: 0 ~ 4, terminate at -1 and 5, 0 and 1 for rewards, respectably"""
-
-        result = [2]
-        state = result[-1]
-        while state != -1 and state != 5:
-            step = -1 if random.uniform(0, 1) < 0.5 else 1
-            state += step
-            result.append(state)
-        return result
-
-    def update_MC(self, episode: list[int]) -> None:
-        for state in episode[:-1]:
-            G = 1 if episode[-1] == 5 else 0
-            self.value_table[state] += self.alpha * (G - self.value_table[state])
-
-    def update_tdzero(self, episode: list[int]) -> None:
-        for state, state1 in zip(episode, episode[1:]):
-            reward = 1 if state1 == 5 else 0
-            v1 = 0 if state1 == -1 or state1 == 5 else self.value_table[state1]
-            # gamma = 1.0
-            self.value_table[state] += self.alpha * (reward + v1 - self.value_table[state])
-
-    def one_batch(self) -> None:
-        update_fn = if  
-        for _ in range(n_episodes):
-                episode = self.generate_episode
-            update_fn(episode, value_table, alpha)
-        return value_table
+def generate_episode() -> list[int]:
+    """state: 0 ~ 4, terminate at -1 and 5, 0 and 1 for rewards, respectably"""
+    result = [2]
+    state = result[-1]
+    while state != -1 and state != 5:
+        step = -1 if random.uniform(0, 1) < 0.5 else 1
+        state += step
+        result.append(state)
+    return result
 
 
-def run_batches(
-    update_fn, n_batches=100, n_episodes=100, alpha=0.1
-) -> tuple(list[float], list[float]):
-    value_table_accum = [0.0] * 5
-    for _ in range(n_batches):
-        value_table = one_batch(update_fn, n_episodes, alpha)
+def one_batch(conf: Config) -> None:
+    updatefn = conf.get_update_method()
+    true_values = [x / 6.0 for x in range(1, 6)]
+    for _ in range(conf.n_episodes):
+        episode = generate_episode()
+        updatefn(episode)
+        conf.rmse_series.append(rmse(true_values, conf.V))
 
 
+def run_batches(conf: Config) -> list[float]:
+    total_rmse_series = [0] * conf.n_batches
+    for _ in range(conf.n_batches):
+        conf.reset()
+        one_batch(conf)
+        for i, rmse in enumerate(conf.rmse_series):
+            total_rmse_series[i] += rmse
+    return [rmse / conf.n_batches for rmse in total_rmse_series]
 
 
 def plot1() -> None:
-    td01, _ = run_episodes(update_tdzero, 1)
-    td10, _ = run_episodes(update_tdzero, 10)
-    td100, _ = run_episodes(update_tdzero, 100)
+    conf = Config(update_method="update_tdzero", n_episodes=1)
+    one_batch(conf)
+    plt.plot(conf.V, label="1")
 
-    trueval = [x / 6.0 for x in [1, 2, 3, 4, 5]]
-    plt.plot(td01, label="1")
-    plt.plot(td10, label="10")
-    plt.plot(td100, label="100")
-    plt.plot(trueval, label="true")
+    conf = Config(update_method="update_tdzero", n_episodes=10)
+    one_batch(conf)
+    plt.plot(conf.V, label="10")
+
+    conf = Config(update_method="update_tdzero", n_episodes=100)
+    one_batch(conf)
+    plt.plot(conf.V, label="100")
+
+    true_values = [x / 6.0 for x in range(1, 6)]
+    plt.plot(true_values, label="true")
+
     plt.legend()
     plt.show()
 
 
 def plot2() -> None:
-    _, rms1 = run_episodes(update_MC, 100)
-    plt.plot(rms1, label="MC")
-    _, rms2 = run_episodes(update_tdzero, 100)
-    plt.plot(rms2, label="tdzero, 0.1")
-    _, rms3 = run_episodes(update_tdzero, 100, alpha=0.15)
-    plt.plot(rms3, label="tdzero, 0.15")
-    _, rms4 = run_episodes(update_tdzero, 100, alpha=0.05)
-    plt.plot(rms4, label="tdzero, 0.05")
+    conf = Config(update_method="update_monte_carlo", alpha=0.01)
+    rmse_series = run_batches(conf)
+    plt.plot(rmse_series, label="MC, 0.01")
+
+    conf = Config(update_method="update_monte_carlo", alpha=0.02)
+    rmse_series = run_batches(conf)
+    plt.plot(rmse_series, label="MC, 0.02")
+
+    conf = Config(update_method="update_monte_carlo", alpha=0.03)
+    rmse_series = run_batches(conf)
+    plt.plot(rmse_series, label="MC, 0.03")
+
+    conf = Config(update_method="update_tdzero", alpha=0.1)
+    rmse_series = run_batches(conf)
+    plt.plot(rmse_series, label="TDZero, 0.1")
+
+    conf = Config(update_method="update_tdzero", alpha=0.15)
+    rmse_series = run_batches(conf)
+    plt.plot(rmse_series, label="TDZero, 0.15")
+
+    conf = Config(update_method="update_tdzero", alpha=0.05)
+    rmse_series = run_batches(conf)
+    plt.plot(rmse_series, label="TDZero, 0.05")
 
     plt.legend()
     plt.show()
 
 
-plot1()
-plot2()
+if __name__ == "__main__":
+    conf = Config()
+    print(type(conf.get_update_method()))
+    # plot1()
+    # plot2()
+
+
 # %%
